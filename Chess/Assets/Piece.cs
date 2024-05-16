5eae771fdac22fdacdba397c58575c3c204b2102
Jack Legate <38793743+spyro21@users.noreply.github.com>
2024-05-15 22:23:43 -0600

    unity init

---
+++ Chess/Assets/Piece.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    string pieceType;
+    (int x, int y) position;
+    int team;
+
+    public Piece(int team, string pieceType, (int x, int y) position)
+    {
+        this.pieceType = pieceType;
+        this.position = position;
+        this.team = team;
+    }
+}
---
+++ Chess/Assets/Script/ChessControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessControl : MonoBehaviour
+{
+    Piece[] pieces = new Piece[32];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setup start
+        pieces[0] = new Piece()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
